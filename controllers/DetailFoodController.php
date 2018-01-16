5ca869836682aea7b1086ccbc640d5de4988a250
Huong <you@example.com>
2018-01-16 20:24:03 +0700

    view

--- controllers/DetailFoodController.php
+++ controllers/DetailFoodController.php
@@ -1,11 +1,21 @@
 <?php
 require_once 'Controller.php';
+require_once "model/DetailFoodModel.php";
 
 class DetailFoodController extends Controller{
     
+    //detail-food.php?id=1&alias=banh-canh
     function indexAction(){
+        $model = new DetailFoodModel;
+
+        $id = $_GET['id'];
+        $alias = $_GET['alias'];
+
+        $food = $model->getFoodById($id, $alias);
+        
         $data = [
-            'title'=>'Chi tiết'
+            'title'=>'Chi tiết',
+            'food'=>$food
         ];
         return $this->loadView('detail-food',$data);
     }
--- model/DetailFoodModel.php
+++ model/DetailFoodModel.php
@@ -1,12 +1,14 @@
 <?php
 require_once 'DBConnect.php';
 class DetailFoodModel extends DBConnect{
+
     function getFoodById($id, $alias){
         $sql = "SELECT f.*, p.url FROM foods f
                 INNER JOIN page_url p
                 ON f.id_url = p.id
                 WHERE f.id = $id 
                 AND p.url = '$alias'";
+        return $this->loadOneRow($sql);
     }
 }
 ?>
